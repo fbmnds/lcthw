f58924b1a8afe763c3b582def7910baaf92a9808
fbmnds <friedrich.boeckh@gmail.com>
2012-06-17 13:37:32 +0200

    exercise 24

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CFLAGS=-Wall -g -std=c99 #-pedantic
 CC=llvm-gcc
 
-#all: clean ex1 ex3 ex7 ex8 ex10 ex11 ex13 ex15 ex16 ex17 ex18 ex19 ex22
-all: clean dbg.h ex22
+#all: clean ex1 ex3 ex7 ex8 ex10 ex11 ex13 ex15 ex16 ex17 ex18 ex19 ex22 ex24
+all: clean dbg.h ex24
 
 #ex13: clean ex13.c
 
@@ -15,6 +15,8 @@ ex18: ex18.c
 ex19: ex19.c ex19-obj.c ex19-obj.h ex19.h
 
 ex22: ex22.c ex22-main.c ex22.h
+
+ex24: ex24.c
 
 clean:
 	rm ex? || rm ex?? || echo
---
+++ ex24.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+#include "dbg.h"
+#include <string.h>
+#define NL printf("\n")
+
+#define MAXDATA 11
+
+typedef enum enumint {
+  ONE, TWO, THREE, FOUR, FIVE, OTHER
+} enumint;
+
+const char * ENUMINTNAMES[] = {
+  "ONE", "TWO", "THREE", "FOUR", "FIVE", "OTHER"
+};
+
+struct results {
+  char name[MAXDATA];
+  int value;
+  enumint prop;
+};
+
+int main (int argc, char * argv[])
+{
+  struct results res;
+  char * rc_cptr;
+  int rc;
+
+  NL; printf("give name[MAXDATA = %d]: ", MAXDATA - 1);
+  /*  rc = fscanf(stdin, "%s", res.name);  */ /* !! unsafe !! */
+  rc_cptr = fgets(res.name, MAXDATA, stdin);
+  check(rc_cptr, "name fscanf error");
+
+  while (getchar() != '\n');  /* eat all until (including) '\n' */
+
+  if (rc_cptr[MAXDATA - 1] == '\0' && res.name[MAXDATA - 1] == '\0')
+    log_info("rc_cptr[MAXDATA - 1] = '\\0', res.name[MAXDATA - 1] = '\\0'");
+  else
+    log_info("rc_cptr[MAXDATA - 1] = '%c', res.name[MAXDATA - 1] = '%c'",
+	   rc_cptr[MAXDATA - 1], res.name[MAXDATA - 1]);
+
+  log_info("res.name = %s of strlen = %ld chars",
+	   res.name, (long) strlen(res.name));
+  log_info("address rc_cptr = %ld, address res.name = %ld", 
+	   (long) rc_cptr, (long) &res.name);
+  NL;
+
+  for (int i = ONE; i <= OTHER; i++)
+    printf("%d) %s\n", i + 1, ENUMINTNAMES[i]);
+  do {
+  printf(" > ");
+  rc = fscanf(stdin, "%d", (int *) &res.prop);
+  check(rc, "error while reading result property");
+  res.prop--;
+  } while (res.prop < 0 || res.prop > OTHER);
+  NL; 
+  log_info("res.prop = ENUMINTNAMES[%d] = %s", 
+	   res.prop, ENUMINTNAMES[res.prop]);
+  NL;
+
+
+
+  return 0; 
+ error:
+  return 1;
+}
